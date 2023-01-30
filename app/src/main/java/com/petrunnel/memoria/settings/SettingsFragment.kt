9c82820ef21d6ff9c5f4015a148c3ba7d3ae9a10
pavel <pavelkotylo@gmail.com>
2023-01-30 22:52:22 +0300

    Added triples and refresh

--- app/src/main/java/com/petrunnel/memoria/settings/SettingsFragment.kt
+++ app/src/main/java/com/petrunnel/memoria/settings/SettingsFragment.kt
@@ -11,23 +11,28 @@ class SettingsFragment : PreferenceFragmentCompat(), OnPreferenceChangeListener 
     private var collection: ListPreference? = null
     private var color: ListPreference? = null
     private var size: ListPreference? = null
+    private var type: ListPreference? = null
 
     private val pictValue: Array<CharSequence> by lazy { collection!!.entries }
     private val colValue: Array<CharSequence> by lazy { color!!.entries }
     private val sizeValue: Array<CharSequence> by lazy { size!!.entries }
+    private val typeValue: Array<CharSequence> by lazy { type!!.entries }
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference, rootKey)
         collection = findPreference("PictureCollection")
         color = findPreference("BackgroundColor")
         size = findPreference("FieldSize")
+        type = findPreference("GameType")
 
         collection?.onPreferenceChangeListener = this
         color?.onPreferenceChangeListener = this
         size?.onPreferenceChangeListener = this
+        type?.onPreferenceChangeListener = this
 
         collection?.summary = collection?.entry
         color?.summary = color?.entry
         size?.summary = size?.entry
+        type?.summary = type?.entry
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
@@ -48,6 +53,12 @@ class SettingsFragment : PreferenceFragmentCompat(), OnPreferenceChangeListener 
         if (key == "FieldSize") {
             val i = (preference as ListPreference).findIndexOfValue(newValue.toString())
             preference.setSummary(sizeValue[i])
+            return true
+        }
+
+        if (key == "GameType") {
+            val i = (preference as ListPreference).findIndexOfValue(newValue.toString())
+            preference.setSummary(typeValue[i])
             return true
         }
 
